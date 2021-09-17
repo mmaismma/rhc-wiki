3517e7e3d07d2d07c878e9706ff8936950de1e3e
mmaismma <mmaismma@hotmail.com>
2021-09-17 19:52:38 +0530

    [Describer] Many changes (list in description) Added episode tracker support Notes can now be colored Yellow, Pink and Lime, Quality Of Life Notes viewed are now saved in sessionStorage for quick access Moved all XMLHttpRequests to fetch() Now using smart jquery style element getter Hermit and season links now point to the edit page

--- src/describe/script.js
+++ src/describe/script.js
@@ -1,38 +1,52 @@
+const $ = (x, all) => {
+    if (all) return document.querySelectorAll(x, true);
+    return document.querySelector(x)
+}
+
 const UI = {
     showFile(e) {
         UI.notes.selectedNote = null;
         UI.loading(e.target, true);
-        firebase.storage().ref().child(e.target.dataset.fullpath).getDownloadURL().then(url => {
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', url)
-            xhr.addEventListener('readystatechange', () => {
-                if (xhr.readyState === 4) {
-                    try {
-                        let json = JSON.parse(xhr.response);
-                        UI.notes.selectedNote = {
-                            elm: e.target,
-                            message: json
-                        };
-                        UI.loading(e.target, false)
-                    } catch (error) {
+        try {
+            let sessionData = sessionStorage.getItem(e.target.dataset.fullpath)
+            if (sessionData) {
+                console.log(sessionData)
+                UI.notes.selectedNote = {
+                    elm: e.target,
+                    message: JSON.parse(sessionData)
+                };
+                UI.loading(e.target, false)
+            } else {
+                firebase.storage().ref().child(e.target.dataset.fullpath).getDownloadURL().then(url => {
+                    fetch(url).then(value => {
+                        if (value.status != 200) {
+                            UI.showMessage(`There is an error with this file, status: ${value.status}`)
+                            return;
+                        }
+                        value.json().then(data => {
+                            UI.notes.selectedNote = {
+                                elm: e.target,
+                                message: data
+                            };
+                            UI.loading(e.target, false)
+                        })
+                    }).catch(error => {
                         UI.showMessage(error)
                         UI.notes.selectedNote = {
                             elm: e.target,
                             message: null
                         };
                         UI.loading(e.target, false)
-                    }
-                }
-            })
-            xhr.send();
-
-        }).catch(error => {
-            UI.showMessage(error)
-            UI.loading(e.target, false)
-        })
+                    })
+                })
+            }
+        } catch (error) {
+                UI.showMessage(error)
+                UI.loading(e.target, false)
+            }
     },
     showMessage(msg) {
-        document.getElementsByTagName('logger')[0].textContent += '>:' + msg + '\n';
+        $('logger').textContent += '>:' + msg + '\n';
     },
     loading(elm, loading = 'toggle', blockPointerEvents = true, colorArray = []) {
         switch (loading) {
@@ -63,71 +77,123 @@ const UI = {
             }
         }
     },
+    updateNoteFormattings() {
+        let currentYellows = localStorage.getItem('describer-yellowed') || '';
+        currentYellows = currentYellows.split(',');
+
+        let currentPinks = localStorage.getItem('describer-pinked') || '';
+        currentPinks = currentPinks.split(',');
+        [...$('nav').children].forEach(x => {
+            if (currentYellows.includes(x.textContent)) {
+                x.classList.add('yellowed')
+            } else {
+                x.classList.remove('yellowed')
+            }
+            if (currentPinks.includes(x.textContent)) {
+                x.classList.add('pinked')
+            } else {
+                x.classList.remove('pinked')
+            }
+            UI.currentYellows = currentYellows.join();
+            UI.currentPinks = currentPinks.join();
+        })
+    },
+    currentYellows: localStorage.getItem('describer-yellowed') || '',
+    currentPinks: localStorage.getItem('describer-pinked') || '',
     notes: new Proxy({
         selectedNote: null,
         allNotes: [],
         refresh() {
-            previewer.value = '';
-            UI.loading(document.getElementsByTagName('nav')[0], true)
+            $('#file-preview').value = '';
+            UI.loading($('nav'), true)
             firebase.storage().ref().child(`describers/user-data/${FIREBASE.user.uid}/`).listAll().then(result => {
                 console.log(result)
                 UI.notes.allNotes = result.items;
-                UI.loading(document.getElementsByTagName('nav')[0], false)
+                UI.loading($('nav'), false)
+                UI.updateNoteFormattings()
             }).catch(error => {
                 UI.showMessage(error)
-                UI.loading(document.getElementsByTagName('nav')[0], false)
+                UI.loading($('nav'), false)
             })
         }
     }, {
         set: (target, property, value, reciever) => {
             if (property === 'selectedNote') {
-                document.querySelector('related-links').innerHTML = 'Related Links:'
+                $('related-links').innerHTML = 'Related Links:'
                 if (target[property]) {
                     target[property].elm.classList.remove('selected')
                 }
                 if (value) {
                     console.log(value);
                     value.elm.classList.add('selected')
-                    previewer.value = value.message?.message || value.message || 'Defected File.';
+                    $('#file-preview').value = value.message?.message || value.message || 'Defected File.';
 
                     if (value.message?.hermit && value.message?.hermit !== '--None--' && value.message?.hermit !== 'none') {
                         let hermitButton = document.createElement('a');
                         hermitButton.textContent = value.message.hermit;
-                        hermitButton.href = `https://reddit.com/r/Hermitcraft/wiki/${value.message.hermit}`;
+                        hermitButton.href = `https://reddit.com/r/Hermitcraft/wiki/edit/${value.message.hermit}`;
                         hermitButton.target = '_blank';
                         hermitButton.title = `Open ${value.message.hermit}'s wiki page in new tab`;
-                        document.querySelector('related-links').append(hermitButton)
+                        $('related-links').append(hermitButton)
                     }
                     if (value.message?.season && value.message?.season !== '--None--' && value.message?.hermit !== 'none') {
                         let seasonButton = document.createElement('a');
                         seasonButton.textContent = `Season ${value.message.season}`;
-                        seasonButton.href = `https://reddit.com/r/Hermitcraft/wiki/season_${value.message.season}`;
+                        seasonButton.href = `https://reddit.com/r/Hermitcraft/wiki/edit/season_${value.message.season}`;
                         seasonButton.target = '_blank';
                         seasonButton.title = `Open season ${value.message.season} wiki page in new tab`;
-                        document.querySelector('related-links').append(seasonButton)
+                        $('related-links').append(seasonButton)
                     }
+                    console.log(value)
+                    sessionStorage.setItem(value.elm.dataset.fullpath, JSON.stringify(value.message))
         
-                    document.getElementById('invalid').disabled = false;
-                    document.getElementById('skip').disabled = false;
-                    document.getElementById('done').disabled = false;
+                    $('#invalid').disabled = false;
+                    $('#skip').disabled = false;
+                    $('#done').disabled = false;
                 } else {
-                    previewer.value = '';
-                    document.getElementById('invalid').disabled = true;
-                    document.getElementById('skip').disabled = true;
-                    document.getElementById('done').disabled = true;
+                    $('#file-preview').value = '';
+                    $('#invalid').disabled = true;
+                    $('#skip').disabled = true;
+                    $('#done').disabled = true;
                 }
                 target[property] = value;
             }
             if (property === 'allNotes') {
                 target.selectedNote = null;
                 if (Array.isArray(value)) {
-                    document.getElementsByTagName('nav')[0].innerHTML = '';
+                    $('nav').innerHTML = '';
                     value.forEach(file => {
                         let tempButton = document.createElement('button');
                         tempButton.textContent = file.name;
                         tempButton.dataset.fullpath = file.fullPath;
-                        tempButton.onclick = UI.showFile;
-                        document.getElementsByTagName('nav')[0].append(tempButton);
+                        tempButton.onclick = (e) => {
+                            if (e.target.classList.contains('selected')) {
+                                let currentYellows = UI.currentYellows;
+                                currentYellows = currentYellows.split(',')
+                                if (currentYellows.includes(e.target.textContent)) {
+                                    currentYellows.splice(currentYellows.indexOf(e.target.textContent), 1);
+                                } else {
+                                    currentYellows.push(e.target.textContent)
+                                }
+                                localStorage.setItem('describer-yellowed', currentYellows.join())
+                                UI.updateNoteFormattings()
+                            } else {
+                                UI.showFile(e);
+                            }
+                        };
+                        tempButton.ondblclick = (e) => {
+                            console.log('aaaa')
+                            let currentPinks = UI.currentPinks;
+                            currentPinks = currentPinks.split(',')
+                            if (currentPinks.includes(e.target.textContent)) {
+                                currentPinks.splice(currentPinks.indexOf(e.target.textContent), 1);
+                            } else {
+                                currentPinks.push(e.target.textContent)
+                            }
+                            localStorage.setItem('describer-pinked', currentPinks.join())
+                            UI.updateNoteFormattings()
+                        }
+                        $('nav').append(tempButton);
                     });
                 }
             }
@@ -140,31 +206,31 @@ const FIREBASE = {
         google: new firebase.auth.GoogleAuthProvider()
     },
     async authenticate(provider) {
-        UI.loading(document.getElementById('sign-in-google'), true)
+        UI.loading($('#sign-in-google'), true)
         try {
             let result = await firebase.auth().signInWithPopup(provider);
             let credential = result.credential;
             let token = credential.accessToken;
             let user = result.user;
-            document.getElementById('sign-in-google').textContent = `Sign out of ${user.displayName}`;
+            $('#sign-in-google').textContent = `Sign out of ${user.displayName}`;
 
-            UI.loading(document.getElementById('sign-in-google'), false)
+            UI.loading($('#sign-in-google'), false)
             UI.showMessage(`Successfully signed in as ${user.displayName} (${user.email})`)
         } catch (error) {
-            UI.loading(document.getElementById('sign-in-google'), false)
+            UI.loading($('#sign-in-google'), false)
             UI.showMessage(error)
         }
 
     },
     signOut() {
-        UI.loading(document.getElementById('sign-in-google'), true)
+        UI.loading($('#sign-in-google'), true)
         firebase.auth().signOut().then(() => {
-            document.getElementById('sign-in-google').textContent = 'Sign in with Google';
+            $('#sign-in-google').textContent = 'Sign in with Google';
             UI.notes.refresh();
-            UI.loading(document.getElementById('sign-in-google'), false)
+            UI.loading($('#sign-in-google'), false)
             UI.showMessage('Successfully signed out')
         }).catch((error) => {
-            UI.loading(document.getElementById('sign-in-google'), false)
+            UI.loading($('#sign-in-google'), false)
             UI.showMessage(error.message)
         });
     },
@@ -179,16 +245,14 @@ const FIREBASE = {
     async moveFirebaseFile(currentPath, destinationPath, fileContent) {
         let message = '';
         if (!fileContent) {
-            let url = await firebase.storage().ref().child(currentPath).getDownloadURL()
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', url, false)
             try {
-                xhr.send();
-                if (xhr.status !== 200) {
-                    UI.showMessage(`Error ${xhr.status}: ${xhr.statusText}`)
-                } else {
-                    message = xhr.response;
+                let url = await firebase.storage().ref().child(currentPath).getDownloadURL()
+                let value = await fetch(url)
+                if (value.status != 200) {
+                    UI.showMessage(`There is an error with this file, status: ${value.status}`)
+                    return;
                 }
+                message = value.json()
             } catch (error) {
                 UI.showMessage(error)
             }
@@ -205,44 +269,63 @@ const FIREBASE = {
         }
     },
     user: null
-
 }
 
-let previewer = document.getElementById('file-preview');
 
-UI.loading(document.getElementById('sign-in-google'), true)
+UI.loading($('#sign-in-google'), true)
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         FIREBASE.user = user;
-        document.getElementById('sign-in-google').textContent = `Sign out of ${user.displayName}`
-        document.getElementById('sign-in-google').onclick = () => {
+        $('#sign-in-google').textContent = `Sign out of ${user.displayName}`
+        $('#sign-in-google').onclick = () => {
             FIREBASE.signOut()
         }
         UI.notes.refresh()
-        UI.loading(document.getElementById('sign-in-google'), false)
+        UI.loading($('#sign-in-google'), false)
     } else {
-        document.getElementById('sign-in-google').onclick = () => {
+        $('#sign-in-google').onclick = () => {
             FIREBASE.authenticate(FIREBASE.authProviders.google)
         }
-        UI.loading(document.getElementById('sign-in-google'), false)
+        UI.loading($('#sign-in-google'), false)
     }
 });
 
-document.getElementById('done').onclick = async (e) => {
-    setTimeout(() => UI.loading(document.getElementById('done'), true, undefined, ['#00c728', '#238636']), 0)
-    await FIREBASE.moveFirebaseFile(UI.notes.selectedNote.elm.dataset.fullpath, `describers/bucket/finished/${FIREBASE.user.uid}/${UI.notes.selectedNote.elm.textContent}`)
-    UI.notes.refresh()
-    setTimeout(() => UI.loading(document.getElementById('done'), false), 0)
+$('#done').onclick = async (e) => {
+    setTimeout(() => UI.loading($('#done'), true, undefined, ['#00c728', '#238636']), 0)
+    FIREBASE.moveFirebaseFile(UI.notes.selectedNote.elm.dataset.fullpath, `describers/bucket/finished/${FIREBASE.user.uid}/${UI.notes.selectedNote.elm.textContent}`).then((res, err) => {
+        if (err) {
+            UI.showMessage(err)
+            UI.notes.refresh()
+            setTimeout(() => UI.loading($('#done'), false), 0)
+            return;
+        }
+        console.log(UI.notes.selectedNote)
+        firebase.firestore().collection(`/episode-tracker-s${UI.notes.selectedNote.message.season}`).doc(UI.notes.selectedNote.message.hermit).collection(`episode${UI.notes.selectedNote.message.episode}`).doc("describers").set({
+            id: FIREBASE.user.uid,
+            time: new Date * 1,
+            noteName: UI.notes.selectedNote.elm.textContent
+        }).then((res, err) => {
+            if (err) {
+                UI.showMessage(err)
+                UI.notes.refresh()
+                setTimeout(() => UI.loading($('#done'), false), 0)
+                return;
+            }
+            UI.showMessage('Data successfully sent to the server, thank you!')
+            UI.notes.refresh()
+            setTimeout(() => UI.loading($('#done'), false), 0)
+        })
+    })
 }
-document.getElementById('skip').onclick = async (e) => {
-    setTimeout(() => UI.loading(document.getElementById('skip'), true), 0)
+$('#skip').onclick = async (e) => {
+    setTimeout(() => UI.loading($('#skip'), true), 0)
     await FIREBASE.moveFirebaseFile(UI.notes.selectedNote.elm.dataset.fullpath, `describers/bucket/skipped/${FIREBASE.user.uid}/${UI.notes.selectedNote.elm.textContent}`)
     UI.notes.refresh()
-    setTimeout(() => UI.loading(document.getElementById('skip'), false), 0)
+    setTimeout(() => UI.loading($('#skip'), false), 0)
 }
-document.getElementById('invalid').onclick = async (e) => {
-    setTimeout(() => UI.loading(document.getElementById('invalid'), true, undefined, ['#f8513d', '#b52f20']), 0)
+$('#invalid').onclick = async (e) => {
+    setTimeout(() => UI.loading($('#invalid'), true, undefined, ['#f8513d', '#b52f20']), 0)
     await FIREBASE.moveFirebaseFile(UI.notes.selectedNote.elm.dataset.fullpath, `describers/bucket/invalid/${FIREBASE.user.uid}/${UI.notes.selectedNote.elm.textContent}`)
     UI.notes.refresh()
-    setTimeout(() => UI.loading(document.getElementById('invalid'), false), 0)
+    setTimeout(() => UI.loading($('#invalid'), false), 0)
 }
